5d7625b8c38d3b204067672231e502f8bb20599a
Mark Haines <mjark@negativecurvature.net>
2016-11-02 15:07:39 +0000

    Implement attachment encryption/decryption (#1)
    * Implement attachment encryption/decryption
    
    * Fix exports
    
    * Explain how the library should be loaded
    
    * Fix package.json URLs
    
    * Explain more of how btoa and atob work
    
    * /a/an/
    
    * comment on what the python file does
    
    * Apparently Array.forEach is a "thing"
    
    * Document the limitations of the assertEq function
    
    * Document that we allow padded base64 in decodeBase64
    
    * Document the keys under 'info'
    
    * Use Math.floor(x) rather than x|0
    
    * Check that the info keys are defined

---
+++ test/generate_test_vectors.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+
+"""
+Generate test vectors for decryption tests
+
+Needs the cryptography module installed to use:
+
+    pip install cryptography
+"""
+
+from cryptography.hazmat.backends import default_backend
+
+from cryptography.hazmat.primitives.ciphers import (
+    Cipher, algorithms, modes
+)
+
+import hashlib
+import json
+import sys
+import base64
+
+b64 = lambda x: base64.b64encode(x).rstrip("=")
+b64u = lambda x: base64.urlsafe_b64encode(x).rstrip("=")
+
+def encrypt(key, iv, plaintext):
+    encryptor = Cipher(
+        algorithms.AES(key),
+        modes.GCM(iv),
+        backend=default_backend()
+    ).encryptor()
+
+    ciphertext = encryptor.update(plaintext) + encryptor.finalize()
+    ciphertext += encryptor.tag
+
+    info = {
+        "key": {
+            "k": b64u(key),
+            "alg": "A256GCM",
+            "kty": "oct",
+            "key_ops": ["encrypt", "decrypt"],
+        },
+        "iv": b64(iv),
+        "hashes": { "sha256": b64(hashlib.sha256(ciphertext).digest()) }
+    }
+    return b64(ciphertext), info, b64(plaintext)
+
+json.dump([
+    encrypt("\x00"*32, "\x00"*16, ""),
+    encrypt("\xFF"*32, "\xFF"*16, "Hello, World"),
+], sys.stdout)
